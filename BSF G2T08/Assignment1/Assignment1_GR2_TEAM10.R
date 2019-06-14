a07135919eb157702351367a15212258d1bb3195
cart3ch <karthigeyan.r@hotmail.com>
2019-06-14 12:40:26 +0530

    Initializing the Group Assignment folder

---
+++ BSF G2T08/Assignment1/Assignment1_GR2_TEAM10.R
@@ -0,0 +1,6 @@
+#Seting up the correct directory
+#setwd("~/PGP-iimt/Term4/BSF gr2/bsf_iimtrichy_18-20/BSF G2T08/Assignment1/")
+
+#Group No.10
+#Members : Karthigeyan 1801093, Paras 1801035, Shanmugapriya 1801115, Niranjan 1801122
+
