4761f03e12bf319c4da7119923d1eb8ba10a97b2
cart3ch <karthigeyan.r@hotmail.com>
2019-08-03 20:40:39 +0530

    Project start

---
+++ BSF G2T10/Project/project.R
@@ -0,0 +1,12 @@
+#######################################################################################
+# BSF Group 2 Team No.10
+# Members : Paras 1801035, Karthigeyan 1801093, Shanmugapriya 1801115, Niranjan 1801122, Raju 1801166
+# Project : Logistic Regression
+#######################################################################################
+
+### Seting up the correct directory
+setwd("BSF G2T10/Project/")
+
+### Data Cleaning
+train<-read.csv("dota2Train.csv")
+test<-read.csv("dota2Test.csv")
